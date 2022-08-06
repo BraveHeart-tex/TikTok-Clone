dcff6d8d6f7f4a835cf13dd2741ba10dfdbbcb7d
Bora <bora.karaca@ogr.iu.edu.tr>
2022-08-06 18:46:28 +0300

    finished upload functionality. created detail page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -37,32 +37,6 @@ function Sidebar() {
               </div>
             </Link>
           </div>
-          {!userProfile && (
-            <div className='px-2 py-4 hidden xl:block'>
-              <p className='text-gray-400'>
-                Log in to like and comment on videos
-              </p>
-              <div className='pr-4'>
-                <GoogleLogin
-                  clientId=''
-                  render={(renderProps) => {
-                    return (
-                      <button
-                        className='bg-white text-lg text-[#f51997] border-[1px] border-[#f51997] font-semibold px-6 py-3 rounded-md outline-one w-full mt-3 hover:text-white hover:bg-[#f51997] transition duration-200 ease-in-out cursor-pointer'
-                        onClick={renderProps.onClick}
-                        disabled={renderProps.disabled}
-                      >
-                        Login
-                      </button>
-                    );
-                  }}
-                  onSuccess={() => {}}
-                  onFailure={() => {}}
-                  cookiePolicy={'single_host_origin'}
-                />
-              </div>
-            </div>
-          )}
           <Discover />
           <SuggestedAccounts />
           <Footer />
